1d63a34f9970a358e59c93cfbf4d383e5c2d4ade
Hoda <hoda.salim11@gmail.com>
2023-07-22 22:19:27 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,15 +6,15 @@
  * */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned long i, j;
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size - 1; j++)
-			{
-				int temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
-			}
+	unsigned long i;
+	for (i = 0; i < size - 1; i++)
+	{
+		if (array[i] > array[i + 1]) {
+		
+			int temp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = temp;
+			print_array(array, size);
 		}
+	}
 }
