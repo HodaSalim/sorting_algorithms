6d223340e3d30a25d993d45a60f49feb963f43c6
Hoda <hoda.salim11@gmail.com>
2023-07-22 22:21:49 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,15 +2,16 @@
 #include "sort.h"
 /*
  * bubble_sort: performs bubble sort on arrays of integers
- * Return: the original array, the array after each sorting phase and the final sorted array
- * */
+ * Return: the original array, the array after each sorting
+ * phase and the final sorted array
+ */
 void bubble_sort(int *array, size_t size)
 {
 	unsigned long i;
 	for (i = 0; i < size - 1; i++)
 	{
-		if (array[i] > array[i + 1]) {
-		
+		if (array[i] > array[i + 1])
+		{
 			int temp = array[i];
 			array[i] = array[i + 1];
 			array[i + 1] = temp;
