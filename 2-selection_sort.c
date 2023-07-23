df3790c78fb566c160d10d051eeb9deeb2694bb9
Hoda <hoda.salim11@gmail.com>
2023-07-23 14:09:27 +0300

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include "sort.h"
 
+/*
+ * Description: performs a selection sort on array of int
+ * retun: void no return value
+ * algo: modifies passed array to decenting order
+ */
+
 void selection_sort(int *array, size_t size)
 {
 	unsigned long i, j;
 
-	for ( i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		unsigned long min_pos = i;
 
 		for (j = i + 1; j < size; j++)
 
-			if(array[j] < array[min_pos]) min_pos = j;
+			if (array[j] < array[min_pos])
+				min_pos = j;
 
-		if(min_pos != i)
+		if (min_pos != i)
 		{
 			int temp = array[i];
+
 			array[i] = array[min_pos];
 			array[min_pos] = temp;
 		}
